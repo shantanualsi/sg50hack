8a6fbca46b2599aae6cdd58b074c5ea2b988d693
Naman Aggarwal <naman.aggarwal@yahoo.com>
2015-07-25 19:59:30 +0800

    Added files for Beacon Detection

--- sghack-frontend/sentosapp/platforms/android/assets/www/cordova_plugins.js
+++ sghack-frontend/sentosapp/platforms/android/assets/www/cordova_plugins.js
@@ -4,12 +4,20 @@ module.exports = [
         "file": "plugins/cordova-plugin-whitelist/whitelist.js",
         "id": "cordova-plugin-whitelist.whitelist",
         "runs": true
+    },
+    {
+        "file": "plugins/pl.makingwaves.estimotebeacons/plugin/src/js/EstimoteBeacons.js",
+        "id": "pl.makingwaves.estimotebeacons.EstimoteBeacons",
+        "clobbers": [
+            "estimote"
+        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "cordova-plugin-whitelist": "1.0.0"
+    "cordova-plugin-whitelist": "1.0.0",
+    "pl.makingwaves.estimotebeacons": "0.7.1"
 }
 // BOTTOM OF METADATA
 });
